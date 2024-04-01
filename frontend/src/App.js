43d4e7a46175fb35f125167d6bb0536ca465138a
Marcel-aka-Satum <wilczynskimarceli@gmail.com>
2024-04-01 16:03:16 +0200

    Frontend file setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home} from './Components/import';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

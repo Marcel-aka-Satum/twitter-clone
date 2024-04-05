44797fad28cdac0cd2a1a3d86cc9ddd1120c434b
Marcel-aka-Satum <wilczynskimarceli@gmail.com>
2024-04-05 17:41:30 +0200

    formdata works

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -7,6 +7,7 @@ from auth.auth import authenticate_user, create_access_token
 from datetime import timedelta
 from auth.auth import ACCESS_TOKEN_EXPIRE_MINUTES
 
+
 router = APIRouter()
 
 
@@ -14,6 +15,7 @@ router = APIRouter()
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
+    print(form_data)
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,40 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
 
-    const handleLogin = () => {
-        fetch("http://localhost:8000/api/v1/token", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        });
-    }
+    fetch("http://localhost:8000/api/v1/token", {
+      method: "POST",
+      withCredentials: true,
+      body: formData,
+    });
+  };
 
-    return (
-        <>
-        <div className="bg-[#989696] h-screen flex justify-center items-center">
-            <div className="container bg-white flex flex-col w-96 p-8 rounded h-3/7 justify-between items-center">
-                <h2 className='text-black font-bold'>log in</h2>
-                <form className='flex-col items-center'>
-                    <input className="p-3 border rounded mt-2 w-full" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-                    
-                    <input className="p-3 border rounded mt-2 w-full" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                    
-                    <button className="bg-white text-blue-500 p-2 border rounded mt-5" onClick={handleLogin}>Login</button>
-                </form>
-            </div>
+  return (
+    <>
+      <div className="bg-[#989696] h-screen flex justify-center items-center">
+        <div className="container bg-white flex flex-col w-96 p-8 rounded h-3/7 justify-between items-center">
+          <h2 className="text-black font-bold">log in</h2>
+          <form className="flex-col items-center">
+            <input
+              className="p-3 border rounded mt-2 w-full"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <input
+              className="p-3 border rounded mt-2 w-full"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              className="bg-white text-blue-500 p-2 border rounded mt-5"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </form>
         </div>
-        </>
-        
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;

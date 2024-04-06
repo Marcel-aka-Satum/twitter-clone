e1baa49d6f01331ae7af1fe90b5a6451dc7b6c7e
Marcel-aka-Satum <wilczynskimarceli@gmail.com>
2024-04-06 14:58:58 +0200

    added env variables backend

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PSSWD = os.getenv("DB_PSSWD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test@localhost/twitter"  # change this to your own database url
+SQLALCHEMY_DATABASE_URL = "postgresql://{DB_USER}:{DB_PSSWD}@{DB_HOST}/{DB_NAME}"  # change this to your own database url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

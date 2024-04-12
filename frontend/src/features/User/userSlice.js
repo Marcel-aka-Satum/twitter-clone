0ac6390416e2fdba670de54cb4dfc2fc8f5e2d03
Yassine El Abdellati <yassineelab@hotmail.com>
2024-04-12 12:22:39 +0200

    added, redux for registration

--- frontend/src/features/User/userSlice.js
+++ frontend/src/features/User/userSlice.js
@@ -4,16 +4,26 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: {},
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(loginAsync.fulfilled, (state, action) => {
-        state.user = JSON.stringify(action.payload);
-        //window.location.href = "/";
+        window.location.href = "/";
+        state.error = null;
       })
       .addCase(loginAsync.rejected, (state, action) => {
-        state.user = action.payload;
+        state.error = action.error.message;
+      })
+
+      .addCase(registerAsync.fulfilled, (state, action) => {
+        window.location.href = "/";
+        state.error = null;
+      })
+
+      .addCase(registerAsync.rejected, (state, action) => {
+        state.error = action.error.message;
       });
   },
 });
@@ -28,6 +38,25 @@ export const loginAsync = createAsyncThunk(
     });
     const data = await response.json();
     return data.user.user;
+  }
+);
+
+export const registerAsync = createAsyncThunk(
+  "user/registerAscync",
+  async ({ username, email, password }) => {
+    await fetch("http://localhost:8000/api/v1/users", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+    });
+    return;
   }
 );
 
